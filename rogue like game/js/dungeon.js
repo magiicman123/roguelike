ef6452116c7c5ed6ab9e253a593e28a0b96886b5
AdamE <adamerick32@gmail.com>
2017-03-05 16:56:17 -0600

    Add files via upload
    new updated walls now dont brake need to fix the hole in the wall from stopping u though

--- rogue like game/js/dungeon.js
+++ rogue like game/js/dungeon.js
@@ -2,8 +2,48 @@
 //audio.play();
 ctx.strokeStyle = 'white';
 ctx.lineWidth = 20;
-ctx.beginPath();
-ctx.moveTo(30,30);
-ctx.lineTo(30,600);
-ctx.lineTo(30,30);
+//bottom
+ctx.moveTo(canv.width/2+100,canv.height/2+100);
+ctx.lineTo(canv.width/2-100,canv.height/2+100);	
+ctx.lineTo(canv.width/2-100,canv.height/2-100);
+ctx.lineTo(canv.width/2+100,canv.height/2-100);
+ctx.lineTo(canv.width/2+100,canv.height/2+110);
 ctx.stroke();
+var firstDoor = Math.floor((Math.random()*4)+1)
+
+if(firstDoor == 1){
+	ctx.beginPath();
+ctx.lineWidth ='30';
+ctx.strokeStyle="black";
+ctx.rect(canv.width/2,canv.height/2-100,20,6); 
+ctx.stroke();
+var secondOne = true;
+}
+if(firstDoor ==2){
+	ctx.beginPath();
+ctx.lineWidth ='30';
+ctx.strokeStyle="black";
+ctx.rect(canv.width/2,canv.height/2+100,20,6); 
+ctx.stroke();
+}
+if(firstDoor == 3){
+	ctx.beginPath();
+ctx.lineWidth ='30';
+ctx.strokeStyle="black";
+ctx.rect(canv.width/2+100,canv.height/2,20,6); 
+ctx.stroke();
+}
+if(firstDoor == 4){
+	ctx.beginPath();
+ctx.lineWidth ='30';
+ctx.strokeStyle="black";
+ctx.rect(canv.width/2-100,canv.height/2,20,6); 
+ctx.stroke();
+}
+if(secondOne == true){
+	ctx.beginPath();
+ctx.lineWidth="10";
+ctx.strokeStyle="blue";
+ctx.rect(50,50,150,80);
+ctx.stroke();
+}
